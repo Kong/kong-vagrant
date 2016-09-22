901a1bdc4a4cc86e5a249d01cbdb377eb30960ed
thefosk <marco@mashape.com>
2016-09-22 01:23:59 -0700

    ubuntu version

--- provision.sh
+++ provision.sh
@@ -1,31 +1,43 @@
 #!/bin/bash
 
+set -o errexit
+
 KONG_VERSION=$@
 
 echo "Installing Kong version: $KONG_VERSION"
 
-sudo bash -c "echo 'nameserver 10.0.2.3' >> /etc/resolv.conf"
+# Install Postgres
+sudo apt-get update
+sudo apt-get install -y software-properties-common python-software-properties
+sudo add-apt-repository "deb https://apt.postgresql.org/pub/repos/apt/ precise-pgdg main"
+wget --quiet -O - https://postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add - 
+sudo apt-get update
+sudo apt-get install -y postgresql-9.5
 
-# Adding Datastax Yum repo
-sudo bash -c "cat >> /etc/yum.repos.d/datastax.repo" << EOL
-[datastax] 
-name = DataStax Repo for Apache Cassandra
-baseurl = https://rpm.datastax.com/community
-enabled = 1
-gpgcheck = 0
+# Configure Postgres
+sudo bash -c "cat > /etc/postgresql/9.5/main/pg_hba.conf" << EOL
+local   all             all                                     trust
+host    all             all             127.0.0.1/32            trust
+host    all             all             ::1/128                 trust
 EOL
 
-# Installing Cassandra
-sudo yum install -y java-1.8.0 cassandra22
-sudo /etc/init.d/cassandra restart
-sudo chmod 777 -R /var/lib/cassandra
-sudo /etc/init.d/cassandra restart
+sudo /etc/init.d/postgresql restart
+
+# Create PG user and database
+psql -U postgres <<EOF
+\x
+CREATE USER kong; 
+CREATE DATABASE kong OWNER kong;
+EOF
+
+# Install Kong
+sudo apt-get install -y netcat openssl libpcre3 dnsmasq procps perl
+wget -O kong.deb https://github.com/Mashape/kong/releases/download/$KONG_VERSION/kong-$KONG_VERSION.precise_all.deb
+sudo dpkg -i kong.deb
+rm kong.deb
 
 # Installing other dependencies
-sudo yum install -y wget tar make curl ldconfig gcc pcre-devel openssl-devel unzip git
-
-# Installing Kong
-sudo yum install -y https://github.com/Mashape/kong/releases/download/$KONG_VERSION/kong-$KONG_VERSION.el7.noarch.rpm
+sudo apt-get install -y git curl make pkg-config unzip libpcre3-dev
 
 # Assign permissions to "vagrant" user
 sudo chown -R vagrant /usr/local

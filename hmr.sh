a2c23cfadde3dbc6b3025f775c0d12c7e743a069
Mark Jubenville <ioncache@gmail.com>
2019-12-04 08:39:49 -0500

    Apply suggestions from code review

--- hmr.sh
+++ hmr.sh
@@ -2,13 +2,12 @@
 
 reload_plugins () {
   echo "Reloading Kong Plugins"
-  vagrant ssh -c "cd /kong-plugin; luarocks make permission-middleware-0.1-1.rockspec"
-  vagrant ssh -c "cd /tf/dev; echo "yes" | terraform apply"
+  vagrant ssh -c "cd /kong-plugin; luarocks make permission-middleware*.rockspec"
+  vagrant ssh -c "cd /tf/dev; terraform apply -auto-approve"
 }
 
 export -f reload_plugins
 
 fswatch -0 -xr --event=Updated ../../permission-middleware | xargs -0 -n1 bash -c reload_plugins
-
 
 
